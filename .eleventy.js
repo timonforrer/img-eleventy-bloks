ba4ce7ddd5ca1e441bb186a1244ace04c5203998
timonforrer <timon.forrer@gmail.com>
2021-09-13 07:08:54 +0200

    Config for debug and production

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,22 @@
+const puppeteer = require('./src/puppeteer.js');
+
 module.exports = function(config) {
+  const debug = process.env.NODE_ENV === 'debug';
+
+  config.addPassthroughCopy('./src/assets');
+
+  if (!debug) {
+    config.on('afterBuild', puppeteer);
+  };
+
+
   return {
     dir: {
       data: 'data',
       input: 'src',
       // includes: 'components',
       layouts: 'layouts',
-      output: 'dist'
+      output: debug ? 'dist' : 'prebuild'
     }
   }
 }
