ebba94f9d6a601a966930465f689c8d0b988bfee
timonforrer <timon.forrer@gmail.com>
2021-09-15 13:09:41 +0200

    Does this still work?

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const fg = require('fast-glob');
 
-module.exports = async function() {
+module.exports = async () => {
   const input = './prebuild';
 
   if(!fs.existsSync('./dist/')) {
@@ -16,44 +16,38 @@ module.exports = async function() {
   // exclude index file
   files = files.filter(fileName => fileName !== `${input}/index.html`);
 
-  console.log(files);
-
-  const screenshot = async () => {
-    // process filenames and add info (name of file without suffix/path + file url for browser)
-    files = files.map(file => {
-      return {
-        relative_path: file,
-        name: file.replace(/.html/, '').slice(input.length+1),
-        url: `file://${path.resolve(file)}`
-      }
-    });
-
-    const browserArgs = {
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
-      headless: true
+  // process filenames and add info (name of file without suffix/path + file url for browser)
+  files = files.map(file => {
+    return {
+      relative_path: file,
+      name: file.replace(/.html/, '').slice(input.length+1),
+      url: `file://${path.resolve(file)}`
     }
+  });
 
-    const browser = await chromium.puppeteer.launch(browserArgs);
-
-    const page = await browser.newPage();
-
-    await page.setViewport({
-      width: 1200,
-      height: 627
-    });
-
-    for (const { url, name } of files) {
-      await page.goto(url);
-      await page.screenshot({
-        path: `./dist/${name}.jpg`,
-        type: 'jpeg',
-        quality: 80 
-      });
-    }
-
-    browser.close()
+  const browserArgs = {
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: true
   }
 
-  screenshot(files);
+  const browser = await chromium.puppeteer.launch(browserArgs);
+
+  const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 1200,
+    height: 627
+  });
+
+  for (const { url, name } of files) {
+    await page.goto(url);
+    await page.screenshot({
+      path: `./dist/${name}.jpg`,
+      type: 'jpeg',
+      quality: 80 
+    });
+  }
+
+  browser.close()
 }
